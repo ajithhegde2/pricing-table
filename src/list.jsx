119716312be174172429c313c731e8877428f3ce
ajithhegde2 <ajith.hegde2@gmail.com>
2022-03-14 18:42:51 +0530

    final

--- src/list.jsx
+++ src/list.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import features from "./features";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-
-
-const list = ({providedFeature}) => {
+const list = ({ eligible, notEligible }) => {
   return (
     <ul className="fa-ul">
-            {features.map((ele, index) => {
-              if (providedFeature.find(a => a.includes(ele))) {
-                return (
-                  <li>
-                    <FontAwesomeIcon icon={faCheck} listItem />
-                    {providedFeature[index]}
-                  </li>
-                );
-              }
-              return <li className="text-muted">
-              <FontAwesomeIcon icon={faTimes} listItem />
-              {ele}
-            </li>
-            })}
-          </ul>
+      {
+      notEligible.map((ele) => {
+        return (
+          <li>
+            <FontAwesomeIcon icon={faCheck} listItem />
+            {ele}
+          </li>
+        )
+      })}
+      {notEligible.map((ele) => {
+        return (
+          <li>
+            <FontAwesomeIcon icon={faCheck} listItem />
+            {ele}
+          </li>
+        )
+      })}
+    </ul>
   )
-}
+};
 
-export default list
+export default list;
